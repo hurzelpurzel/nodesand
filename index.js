d35a5576562804be36328c3c350397d5be64fe45
hurzelpurzel <ludger.pottmeier@gmx.de>
2018-03-30 19:58:25 +0200

    secrets

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
+const fs = require('fs');
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/secrets', function (req, res) {
+    var secrets =fs.readdirSync('/secrets');
+    res.send('The secrets are'+ JSON.stringify(secrets));
+  });
 
 app.listen(port, function () {
   console.log('Example app listening on port!'+8080);

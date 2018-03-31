bc2c7efcbb165db81f31235ea29331e0a90f1ddc
hurzelpurzel <ludger.pottmeier@gmx.de>
2018-03-31 10:12:43 +0200

    environment

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
-const secrets = require('./configfiles/secrets');
+//const secrets = require('./configfiles/secrets');
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.get('/secrets', function (req, res) {
-     res.send('The secrets are'+ JSON.stringify(secrets));
-  });
+//app.get('/secrets', function (req, res) {
+//     res.send('The secrets are'+ JSON.stringify(secrets));
+//  });
+
+app.get('/env', function (req, res) {
+    res.send('The env is'+ JSON.stringify(process.env));
+ });  
 
 app.listen(port, function () {
   console.log('Example app listening on port!'+8080);
